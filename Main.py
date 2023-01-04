249f7d49c94a6987339fba14d33b6fa9d2635441
abdirahmansabriye <109628025+abdirahmansabriye@users.noreply.github.com>
2023-01-04 15:58:03 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,6 +16,33 @@ while True:
 
 print ("Bye!")
 
+tasks = []
+
+def add_task(task):
+  tasks.append(task)
+  print("Task added:", task)
+
+def complete_task(task):
+  tasks.remove(task)
+  print("Task completed:", task)
+
+def list_tasks():
+  print("Current tasks:")
+  for task in tasks:
+    print(task)
+
+while True:
+  action = input("What would you like to do? (add/complete/list) ")
+  if action == "add":
+    task = input("Enter a task: ")
+    add_task(task)
+  elif action == "complete":
+    task = input("Enter a task to complete: ")
+    complete_task(task)
+  elif action == "list":
+    list_tasks()
+  else:
+    print("Invalid input.")
 
 
 
